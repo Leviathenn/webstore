20f95e94a1d633c3b7c728b48eaff0ced34a9011
Levi <levixanderson@hotmail.com>
2023-11-16 14:19:08 +0000

    asd

--- main.js
+++ main.js
@@ -25,10 +25,49 @@ ExitCode: 0
 
 //process.exit(0);
 
-const map = process.argv[2]
-if(fs.existsSync(`map/${map}.ucmp`)){
-    //Map exist, prepare for loading...
+let writeString;
+
+const mapf = process.argv[2]
+if(fs.existsSync(`map/${mapf}.ucmp`)){
+    //Map exist, prepare for loading..."
+    if(debugEnabled){
+        console.log(`[-] Map ${mapf} exist! Prepairing to compile...`);
+    
+    }else{
+        //Don't log
+    }
+    const dumpFile = `map/${mapf}.cmp`
+    fs.writeFileSync(dumpFile, "");
+    const map = fs.readFileSync(`map/${mapf}.ucmp`);
+    const mapData = map.toString().split(" ");
+    mapData.forEach(id =>{
+        const mainId = id.split(':')
+        
+        switch (parseInt(mainId)) {
+            case 1:
+                const picture = fs.readFileSync("icons/grass.png");
+                writeString += `:-+@$@
+${picture}
+$#$+_-`
+                
+                break;
+            case 2:
+                const picturez = fs.readFileSync("icons/cobblestone.png");
+                writeString += `:-+@$@
+${picturez}
+$#$+_-`
+                fs.writeFileSync(`map/${mapf}.cmp`, writeString)
+                break;
+            default:
+                console.log(`Error: id: ${id} does not exist.`)
+                process.exit(1)
+                
+                break;
+        }
+    })
+
 }else{
     console.log(`Error: Map ${map} does not exit!`)
     process.exit(1)
 }
+
