ea5a937d89437352681ee0bcb5ea080298be3446
Levi <levixanderson@hotmail.com>
2023-11-16 15:35:17 +0000

    blah

--- testrenderer/main.js
+++ testrenderer/main.js
@@ -3,5 +3,33 @@
  */
 
 $(document).ready(function (){
-    
+    $.ajax({
+        url: "s.cmp",
+        success: function (response) {
+            const eachLine = response.split("\n");
+            lineIndex = 0
+            eachLine.forEach(line => {
+                if(line.startsWith(">")){
+                    mainSplit = line.split(">");
+                    mainInfo = mainSplit[1].split('/')
+                    mainID = mainInfo[0];
+                    cords = mainInfo[1].split('.');
+                    xcord = cords[0];
+                    ycord = cords[1];
+            
+
+                    /**$(".alr").append(`<h1>
+                    mainId: ${mainID}  
+                    cords: ${cords[0]}, ${cords[1]}
+                    xcord: ${xcord}
+                    ycord: ${ycord}
+                    </h1>
+                    <br>
+                    **/`)
+                
+                           
+                }
+            });
+        }
+    });
 })
